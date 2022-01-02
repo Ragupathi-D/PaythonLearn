58ef8208ebda99ca9ec5eb1a7cad86e95f39663c
Ragupathi-D <48428043+Ragupathi-D@users.noreply.github.com>
2022-01-02 15:38:12 +0530

    Update hangman.py

--- game/hangman.py
+++ game/hangman.py
@@ -10,24 +10,21 @@ totalChance = 5
 leterLength = len(question)
 fill = ['_' for _ in range(leterLength)]
 checkWords = [question[i] for i in range(0, leterLength)]
-print(checkWords)
 while(leterLength != correct and totalChance != 0):
-  clear()
-  print(f"Please find the word, {','.join(fill)}")
+  print(f"Please find the word : {','.join(fill)}")
   print(f"Your total chance is {totalChance}")
   userInput = input('Please enter the word : ').lower()
-  check = userInput in checkWords
-  if(not check or userInput != ''):
+  indexes = [ i for i in range(len(checkWords)) if(checkWords[i] == userInput) ]
+  if(len(indexes) == 0 or userInput == ''):
     totalChance-=1
   else:
-    index = checkWords.index(userInput)
-    print('index', index)
-    fill[index] = userInput
-    checkWords[index] = ''
-    correct+=1;
+    for index in indexes:
+      fill[index] = userInput
+      checkWords[index] = ''
+      correct+=1
 
 clear()
-print(f"Please find the word, {','.join(fill)}")
+print(f"Please find the word : {','.join(fill)}")
 
 if(totalChance!= 0):
   print("You Are won")
