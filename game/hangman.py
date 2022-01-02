38494ecc4a7068a188bdc25052a8e58bf23b4557
Ragupathi-D <48428043+Ragupathi-D@users.noreply.github.com>
2022-01-02 15:03:54 +0530

    Create hangman.py

---
+++ game/hangman.py
@@ -0,0 +1,37 @@
+import os
+import random
+
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi']
+
+question = random.choice(fruits)
+clear = lambda: os.system('clear')
+correct = 0
+totalChance = 5
+leterLength = len(question)
+fill = ['_' for _ in range(leterLength)]
+checkWords = [question[i] for i in range(0, leterLength)]
+print(checkWords)
+while(leterLength != correct and totalChance != 0):
+  clear()
+  print(f"Please find the word, {','.join(fill)}")
+  print(f"Your total chance is {totalChance}")
+  userInput = input('Please enter the word : ').lower()
+  check = userInput in checkWords
+  if(not check):
+    totalChance-=1
+  else:
+    index = checkWords.index(userInput)
+    print('index', index)
+    fill[index] = userInput
+    checkWords[index] = '_'
+    correct+=1;
+
+clear()
+print(f"Please find the word, {','.join(fill)}")
+
+if(totalChance!= 0):
+  print("You Are won")
+else:
+  print("You lost")
+
+print('game over')
